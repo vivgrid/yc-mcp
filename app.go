e568334ec28522c999ce2a2de1fbdbe57122a8a8
venjiang <venjiang@gmail.com>
2025-07-09 19:40:04 +0800

    rename

---
+++ app.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"strings"
+
+	"github.com/vivgrid/yc"
+	"github.com/yomorun/yomo/serverless"
+)
+
+const (
+	CmdYC      = "yc"
+	CmdCreate  = "create"
+	CmdDeploy  = "deploy"
+	CmdLogs    = "logs"
+	CmdRemove  = "remove"
+	CmdStart   = "start"
+	CmdStatus  = "status"
+	CmdStop    = "stop"
+	CmdUpload  = "upload"
+	CmdVersion = "version"
+)
+
+// Init is an optional function invoked during the initialization phase of the sfn instance
+func Init() error {
+	return nil
+}
+
+// Description returns a description of the function
+func Description() string {
+	return `yc is the command-line interface (CLI) tool for deploying your serverless functions on Vivgrid. It provides a convenient way to manage your deployments and serverless instances globally.
+
+	To install yc, you can use the following command:
+	'curl "https://bina.egoist.dev/vivgrid/yc" | sh'
+
+	- You can create a 'yc.yml' file in the root of your project to configure the deployment settings.
+	- Return the usage method based on the input command
+	`
+}
+
+// InputSchema defines the argument structure for LLM Function Calling. It
+// utilizes jsonschema tags to detail the definition. For jsonschema in Go,
+// see https://github.com/invopop/jsonschema.
+func InputSchema() any {
+	return &LLMArguments{}
+}
+
+// LLMArguments defines the arguments for the LLM Function Calling. These
+// arguments are combined to form a prompt automatically.
+type LLMArguments struct {
+	Command string `json:"command" jsonschema:"title=Command,description=The command to execute,example=yc deploy"`
+}
+
+// Handler orchestrates the core processing logic of this function.
+// - ctx.ReadLLMArguments() parses LLM Function Calling Arguments (skip if none).
+// - ctx.WriteLLMResult() sends the retrieval result back to LLM.
+func Handler(ctx serverless.Context) {
+	var p LLMArguments
+	ctx.ReadLLMArguments(&p)
+
+	cmd := p.Command
+	switch {
+	// create
+	case strings.Contains(cmd, CmdCreate):
+		cmd = CmdCreate
+		// deploy
+	case strings.Contains(cmd, CmdDeploy):
+		cmd = CmdDeploy
+		// logs
+	case strings.Contains(cmd, CmdLogs):
+		cmd = CmdLogs
+		// remove
+	case strings.Contains(cmd, CmdRemove):
+		cmd = CmdRemove
+		// start
+	case strings.Contains(cmd, CmdStart):
+		cmd = CmdStart
+		// status
+	case strings.Contains(cmd, CmdStatus):
+		cmd = CmdStatus
+		// stop
+	case strings.Contains(cmd, CmdStop):
+		cmd = CmdStop
+		// upload
+	case strings.Contains(cmd, CmdUpload):
+		cmd = CmdUpload
+		// version
+	case strings.Contains(cmd, CmdVersion):
+		cmd = CmdVersion
+	default:
+		cmd = CmdYC
+	}
+
+	doc, err := yc.Doc(cmd)
+	if err != nil {
+		slog.Error("yc-mcp", "command", p.Command, "error", err)
+		ctx.WriteLLMResult(fmt.Sprintf("Error retrieving documentation for command %s: %v", cmd, err))
+		return
+	}
+
+	ctx.WriteLLMResult(doc)
+	slog.Info("yc-mcp", "command", p.Command, "doc", doc)
+}
---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/vivgrid/yc-mcp
+
+go 1.24.2
+
+toolchain go1.24.5
+
+require (
+	github.com/vivgrid/yc v1.13.4
+	github.com/yomorun/yomo v1.20.6
+)
+
+replace github.com/vivgrid/yc => ../yc
+
+require (
+	github.com/sashabaranov/go-openai v1.40.1 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+)
